6474b7d5c934e0f85276f016ec76ff91aeee9af9
aayushimahajan <97786479+aayushimahajan@users.noreply.github.com>
2023-11-11 11:13:23 +0530

    Create ha_helper.h

---
+++ src/hybridautomata/ha_helper.h
@@ -0,0 +1,19 @@
+/*
+ * ha_helper.h
+ *
+ *  Created on: 19-May-2023
+ *      Author: amit
+ */
+
+#ifndef HYBRIDAUTOMATA_HA_HELPER_H_
+#define HYBRIDAUTOMATA_HA_HELPER_H_
+
+
+#include "../hybridautomata/hybridAutomata.h"
+#include "../hybridautomata/location.h"
+#include "../hybridautomata/transition.h"
+
+void verifyHA_output(hybridAutomata::ptr &H);
+
+
+#endif /* HYBRIDAUTOMATA_HA_HELPER_H_ */
